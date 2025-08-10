b9846b1654e1a9ed0e8e7339c2b64b267ed97f45
Uelisson <tagots09@gmail.com>
2025-08-10 13:01:41 -0300

    Botão de criação feito com sucesso

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const controles = document.getElementById("controles");
+const cssText = document.querySelector(".css");
+const btn = document.querySelector(".btn");
+controles.addEventListener("change", handleChange);
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + "px";
+  },
+  width(value) {
+    this.element.style.width = value + "px";
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + "px";
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + "rem";
+  },
+};
+
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  handleStyle[name](value);
+  saveValues(name, value);
+  showCss();
+}
+
+function saveValues(name, value) {
+  localStorage.setItem(name, value);
+}
+
+function setValues() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    if (handleStyle[key] && controles.elements[key]) {
+      handleStyle[key](value);
+      controles.elements[key].value = value;
+    }
+  }
+  showCss();
+}
+
+document.addEventListener("DOMContentLoaded", setValues);
